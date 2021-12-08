372f4a0779d82e09fc931497954ab8ff59afcd7e
Vincent Dandenault <vdandenault@gmail.com>
2021-12-08 08:35:06 -0500

    Notebook done

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 EXPERIMENT = 'indices'  # Name of experiment
 RUN_NAME = 'initial assessment'  # Name of iteration (that will show
 ARTIFACT_DIR = '../artifact_dir'  # Dir for notebook outputs where plots are saved
+EPOCH = 40 #baseline number of epochs
